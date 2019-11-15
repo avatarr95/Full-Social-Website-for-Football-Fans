12feed968c683380b6aa96657b91a0774a36c432
avatarr95 <piter.zaw@live.com>
2019-11-15 13:18:42 +0100

    added remaining last posts

--- mysite/valverde/templatetags/valverde_tags.py
+++ mysite/valverde/templatetags/valverde_tags.py
@@ -30,6 +30,10 @@ def get_2nd_last_post():
 def get_3rd_last_post():
     return Post.objects.filter(status="published").order_by("-publish")[2]
 
+@register.simple_tag
+def get_4_to_24_last_posts():
+    return Post.objects.filter(status="published").order_by('-publish')[1:24]
+
 
 #MOST COMMENTED POSTS TAGS
 
@@ -49,6 +53,11 @@ def get_second_most_commented_post():
 @register.simple_tag
 def most_commented_posts(count=5):
     return Post.objects.filter(status="published", publish__lte=timezone.now(), publish__gte=timezone.now()-datetime.timedelta(days=1)).annotate(total_comments=Count("comments")).order_by("-total_comments")[:count]
+
+
+
+
+
 """
 
 # markdown używamy, gdy jesteśmy pewni, że kod jest bezpieczny... lepiej nie ufać nikomu
