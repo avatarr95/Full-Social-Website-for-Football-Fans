dbe1d6d07f2dfc45eb3ec823c87eb6d4cc4202e9
avatarr95 <piter.zaw@live.com>
2019-12-22 17:46:16 +0100

    changed some settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ckeditor',
-    'storages',
 ]
 
 CKEDITOR_CONFIGS = {
@@ -227,15 +226,6 @@ LOGOUT_URL = 'valverde:logout'
 
 
 
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME') 
 
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
-
-# # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# django_heroku.settings(locals())
 
 
